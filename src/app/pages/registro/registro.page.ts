53f1b8a4e6c732e71ef433ffc0a229228902d043
Lcfz01 <lmze001@gmail.com>
2024-05-17 17:19:11 -0600

    Login listo con registro, faltan mensajes de error

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
+    this.email = '';
+    this.password = '';
+  }
 
   ngOnInit() {
   }
 
+  async registro() {
+    try {
+      const user = await this.afAuth.createUserWithEmailAndPassword(this.email, this.password);
+      console.log('Registro exitoso', user);
+      this.router.navigate(['/lista-qr']);
+    } catch (error) {
+      console.error('Error en el registro', error);
+      // Aquí puedes mostrar un mensaje de error al usuario
+    }
+  }
 }
+
+
